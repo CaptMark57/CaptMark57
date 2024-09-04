fd53bef55f49504732571116d207e7693cbb8b77
CaptMark57 <98343446+CaptMark57@users.noreply.github.com>
2024-09-03 20:36:26 -0500

    Add files via upload

---
+++ 1st night.py
@@ -0,0 +1,13 @@
+print ("Hello World")
+
+print ("what time is it?")
+
+print ("a.5 pm")
+print ("b.6 pm")
+print ("c.7 pm")
+print ("d.8 pm")
+
+answer = 'c'
+
+user_choice = input("pick a letter")
+print(answer==user_choice)
