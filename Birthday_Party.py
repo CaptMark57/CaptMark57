2258c4cb9aaf7418571bdabad9d30f6a37a80803
CaptMark57 <98343446+CaptMark57@users.noreply.github.com>
2024-10-01 18:11:30 -0500

    Add files via upload

---
+++ Birthday_Party.py
@@ -0,0 +1,24 @@
+# Week 2 Lesson 1
+guests = ["Alice", "Bob", "Charlie", "David", "Eve"]
+activities = ("cake cutting", "games", "gift opening", "singing")
+
+nonVegetarian = 0
+vegan = 0
+
+food_preferences = {
+    "Alice": "vegetarian",
+    "Bob": "non-vegetarian",
+    "Charlie": "vegan",
+    "David": "non-vegetarian",
+    "Eve": "vegetarian"
+}
+allergies = {"nuts", "dairy", "gluten"}
+
+print(guests, "\n",activities)
+
+for index in food_preferences:
+    if food_preferences[index] == 'vegetarian'
+
+ if food_preferences[index] == 
+
+ if food_preferences[index] ==
