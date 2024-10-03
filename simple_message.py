3cc01272646e12d3b1233bbe4c9a9e9b2b499b36
CaptMark57 <98343446+CaptMark57@users.noreply.github.com>
2024-10-03 14:30:50 -0500

    Add files via upload

---
+++ simple_message.py
@@ -0,0 +1,6 @@
+# assignment 2-1
+
+message = "hello world!"
+print(message)
+
+#
---
+++ simple_messages.py
@@ -0,0 +1,10 @@
+# Assignment 2-2
+
+message = "hello world!"
+print(message)
+
+message = "hello usa!"
+
+print(message)
+
+
